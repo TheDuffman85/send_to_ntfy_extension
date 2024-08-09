aaaf9f7cb64d792a018e2d5750d0a87a25783af8
meerkatzenwildschein <anderle@sonnenkinder.org>
2024-08-09 19:19:59 +0200

    Replace tabs with spaces

--- popup.js
+++ popup.js
@@ -100,22 +100,22 @@ document.addEventListener('DOMContentLoaded', function() {
         setTimeout(() => { warning.classList.add('hidden'); }, 3000);
         return;
       }
-	  
-	    const urlObj = new URL(apiUrl);
+    
+      const urlObj = new URL(apiUrl);
       const username = urlObj.username;
       const password = urlObj.password;
-	  
+    
       // Remove username and password from url
       urlObj.username = '';
       urlObj.password = '';
-	
+  
       const fullUrl = urlObj.toString() + topic;
-	  
+    
       const headers = new Headers();
-	    if(username && password) {
-	      const credentials = btoa(`${username}:${password}`);
-		    headers.set('Authorization', `Basic ${credentials}`);
-	    }
+      if(username && password) {
+        const credentials = btoa(`${username}:${password}`);
+        headers.set('Authorization', `Basic ${credentials}`);
+      }
       if (accessToken) {
         headers.set('Authorization', `Bearer ${accessToken}`);
       }
